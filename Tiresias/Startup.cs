295b0dffafdc1a8b30b854673d0eccc990ddb0ca
foremaro <foremaro@mail.gvsu.edu>
2016-06-19 10:38:28 -0400

    Add project files.

---
+++ Tiresias/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tiresias.Startup))]
+namespace Tiresias
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
